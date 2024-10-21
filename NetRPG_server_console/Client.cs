718ba32ea3400f911add16e84013319445da03a6
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-10-21 20:57:54 +0900

    #임시 커밋 TO-DO 새 프로젝트 구성 : Server, Client솔루션 여러개 만들어서 테스트 후 결과 확인

---
+++ NetRPG_server_console/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Client
+{
+    static void Main(string[] args)
+    {
+        private TcpClient tc =  = new TcpClient("127.0.0.1", 7000);
+
+        string msg = "Hello World!";
+        byte[] buff = Encoding.UTF8.GetBytes(msg);
+
+        NetworkStream stream = tc.GetStream();
+            
+        stream.Write(buff, 0, buff.Length);
+
+        byte[] outbuff = new byte[1024];
+        int nbytes = stream.Read(outbuff, 0, outbuff.Length);
+        string output = Encoding.UTF8.GetString(outbuff, 0, nbytes);
+
+        stream.Close();
+        tc.Close();
+            
+        Console.WriteLine($"{nbytes} bytes : {output}");
+    }
+    
+}
