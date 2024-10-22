5eb10803c6229ab673fdec7bb4f09cf61070bc38
co2plant <93631841+co2plant@users.noreply.github.com>
2024-10-22 23:07:54 +0900

    #수정을 위한 임시 커밋

--- NetRPG_server_console/Program.cs
+++ NetRPG_server_console/Program.cs
@@ -1,10 +1,44 @@
 ﻿using NetRPG_server_console;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        TcpListenerConsole tcpListenerConsole = new TcpListenerConsole();
-        tcpListenerConsole.Start();
+        int[] arr = new int[24000];
+        arr[0] = 4;
+        int cons = 4;
+        int count = 0;
+        for (int i = 1; i < 24000; i++)
+        {
+            cons += 4;
+            arr[i] = arr[i - 1] + cons;
+        }
+        int inputN = int.Parse(Console.ReadLine());
+        
+
+        for (int i = 0; i < inputN; i++)
+        {
+            int inputM = int.Parse(Console.ReadLine());
+            int j = 0;
+            while (true)
+            {
+                if (((inputM - arr[j])/(j*2+3))==0)
+                {
+                    break;
+                }
+                if(inputM<1152048000)
+                
+                if (inputM < arr[j] || inputM>1152048000)
+                {
+                    count += 1;
+                    break;
+                }
+
+                
+                j++;
+            }
+        }
+
+        Console.WriteLine(count);
     }
 }
---
+++ temp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[24001];
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < 24000; i++)
+        {
+            arr[i] = i * 2 + 1;
+        }
+
+        for (int i = 1; i < 24000; i++)
+        {
+            if(arr[i])
+        }
+    }
+}
