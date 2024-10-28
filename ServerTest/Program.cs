bb0c0deffa61a88dd146b4af02585db866efccc1
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-10-28 22:26:16 +0900

    서버를 위한 임시 커밋추가

--- ServerTest/Program.cs
+++ ServerTest/Program.cs
@@ -1,33 +1,65 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace ServerTest
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            AysncEchoServer().Wait();
+        }
+ 
+        async static Task AysncEchoServer()
+        {           
             TcpListener listener = new TcpListener(IPAddress.Any, 7000);
-            listener.Start();
-
-            byte[] buff = new byte[1024];
-
+            listener.Start();            
             while (true)
             {
-                TcpClient tc = listener.AcceptTcpClient();
-
-                NetworkStream stream = tc.GetStream();
-
-                int nbytes;
-
-                while ((nbytes = stream.Read(buff, 0, buff.Length)) > 0)
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);                
+                 
+                Task.Factory.StartNew(AsyncTcpProcess, tc);                
+            }
+        }
+ 
+        async static void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+ 
+            int MAX_SIZE = 1024;  // 가정
+            NetworkStream stream = tc.GetStream();
+            try
+            {
+                while (true)
                 {
-                    stream.Write(buff, 0, nbytes);
+                    var buff = new byte[MAX_SIZE];
+                    var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+                    if (nbytes > 0)
+                    {
+                        string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                        Console.WriteLine($"{msg} at {DateTime.Now}");
+
+                        // 비동기 송신
+                        await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+                    }
+
+                    stream.Close();
+                    tc.Close();
                 }
-            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+            finally
+            {
                 stream.Close();
                 tc.Close();
             }
+            
         }
     }
 }
