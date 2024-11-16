a7758044592ace611d6d24679e91c2d2cfa37198
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-11-16 23:59:16 +0900

    #단일 책임의 원칙 (SRP) 준수를 위한 ChatServer.cs 수정

--- NetRPG_server_console/ChatServer.cs
+++ NetRPG_server_console/ChatServer.cs
@@ -10,12 +10,11 @@ namespace NetRPG_server_console
     public class ChatServer
     {
         private TcpListener _listener;
-        private Dictionary<string, Room> _rooms = new();
-
+        private RoomManager _roomManager;
         public ChatServer(IPAddress ipAddress, int port)
         {
             _listener = new TcpListener(ipAddress, port);
-            
+            _roomManager = new RoomManager();
         }
 
         public async Task StartAsync()
@@ -36,34 +35,19 @@ namespace NetRPG_server_console
             int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
             string roomTitle = Encoding.UTF8.GetString(buffer, 0, bytesRead);
 
-            if (!_rooms.ContainsKey(roomTitle))
-            {
-                _rooms[roomTitle] = new Room((roomTitle));
-                Console.WriteLine("Room created: " + roomTitle);
-            }
+            Room room = _roomManager.GetOrCreateRoom(roomTitle);
 
-            Room room = _rooms[roomTitle];
             if (!room.IsFull)
             {
                 User user = new User(GenerateUserId(), "User" + GenerateUserId(), client);
                 room.AddUser(user);
-                await BroadcastMessageAsync(roomTitle, $"{client.Client.RemoteEndPoint} joined the room.");
+                await _roomManager.BroadcastMessageAsync(roomTitle, $"{client.Client.RemoteEndPoint} joined the room.");
             }
             else
             {
                 byte[] message = Encoding.UTF8.GetBytes("Room is full.");
                 await stream.WriteAsync(message, 0, message.Length);
                 client.Close();
-            }
-        }
-
-        private async Task BroadcastMessageAsync(string roomTitle, string message)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-            foreach (var user in _rooms[roomTitle].Users)
-            {
-                NetworkStream stream = user.Client.GetStream();
-                await stream.WriteAsync(buffer, 0, buffer.Length);
             }
         }
 
